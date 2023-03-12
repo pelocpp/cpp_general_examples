81d0ca2239284a08add853c689e1ed4217f75cd2
Peter Loos <peter.loos@gmx.de>
2023-03-12 18:31:36 +0100

    added new project 'hash map'

---
+++ Examples/HashMap/Hash.h
@@ -0,0 +1,35 @@
+// ===========================================================================
+// Hash.h // Hash Function
+// ===========================================================================
+
+#pragma once
+
+#include <string>
+
+namespace SimpleHashMap {
+
+	// primary template - default hash function object
+	template <typename T>
+	class Hash
+	{
+	public:
+		size_t operator()(const T& key) const;
+	};
+
+	// template specialization for std::string objects
+	template <>
+	class Hash<std::string>
+	{
+	public:
+		size_t operator()(const std::string& key) const;
+	};
+
+	// TODO: Was ist mit const char*
+	
+}
+
+#include "hash.hpp"
+
+// ===========================================================================
+// End-of-File
+// ===========================================================================
